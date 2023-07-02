5b5c1ce9a4d11836d374b7252f3cbb8c42b5a7ae
Nico <molonjohnnico@gmail.com>
2023-07-02 20:19:01 +0800

    Automate documentation

--- app/src/main/java/com/wkt/distriware/MainActivity.kt
+++ app/src/main/java/com/wkt/distriware/MainActivity.kt
@@ -6,30 +6,61 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.wkt.distriware.ui.compose.NavGraph
 import com.wkt.distriware.ui.theme.DistriwareTheme
+
+/**
+ * The main activity of the Distriware application.
+ *
+ * This activity serves as the entry point for the application and hosts the main UI components.
+ * It extends the [ComponentActivity] class and is responsible for initializing the app's content and UI.
+ *
+ * ## Usage
+ *
+ * To use the `MainActivity`, add it to your app's manifest file and ensure it is the launch activity.
+ *
+ * In the `onCreate` method, the activity sets the content view using the Compose API and applies the
+ * `DistriwareTheme` to style the UI. It then creates a [Surface] with the specified [Modifier] and
+ * background color from the [MaterialTheme.colorScheme], and finally, it displays the app's [NavGraph].
+ *
+ * ```kotlin
+ * class MainActivity : ComponentActivity() {
+ *     override fun onCreate(savedInstanceState: Bundle?) {
+ *         super.onCreate(savedInstanceState)
+ *         setContent {
+ *             DistriwareTheme {
+ *                 Surface(
+ *                     modifier = Modifier.fillMaxSize(),
+ *                     color = MaterialTheme.colorScheme.background
+ *                 ) {
+ *                     NavGraph()
+ *                 }
+ *             }
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * ## Remarks
+ *
+ * - The `MainActivity` should be declared in the app's manifest file with an intent filter specifying
+ *   `android.intent.action.MAIN` and `android.intent.category.LAUNCHER` to make it the launch activity.
+ * - The `DistriwareTheme` is a custom theme defined in the app that provides a consistent look and feel
+ *   for the UI components.
+ * - The `NavGraph` is a Compose UI component responsible for managing the app's navigation and displaying
+ *   the appropriate screens based on the user's interaction.
+ */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DistriwareTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     NavGraph()
                 }
             }
         }
-    }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    DistriwareTheme {
-        NavGraph()
     }
 }
--- app/src/main/java/com/wkt/distriware/data/repository/ConfigSettingRepository.kt
+++ app/src/main/java/com/wkt/distriware/data/repository/ConfigSettingRepository.kt
@@ -2,6 +2,28 @@ package com.wkt.distriware.data.repository
 
 import com.wkt.distriware.domain.model.ConfigSetting
 
+/**
+ * ## Interface for configuration setting
+ *
+ * The `ConfigSettingRepository` interface provides methods for accessing and managing configuration settings.
+ *
+ * ### Usage
+ *
+ * To use the `ConfigSettingRepository`, obtain an instance and call its methods as needed.
+ *
+ * ```kotlin
+ * val repository = ConfigSettingRepositoryImpl()
+ * val configSetting = repository.getConfigSetting()
+ * val isEmpty = repository.isConfigSettingEmpty()
+ * val isValid = repository.isConfigSettingValid()
+ * ```
+ *
+ * ### Remarks
+ *
+ * - The `getConfigSetting` method returns the current configuration setting as a [ConfigSetting] object.
+ * - The `isConfigSettingEmpty` method returns `true` if the configuration setting is empty; otherwise, it returns `false`.
+ * - The `isConfigSettingValid` method checks if the configuration setting is valid and returns a Boolean value.
+ */
 interface ConfigSettingRepository {
     fun getConfigSetting(): ConfigSetting
     fun isConfigSettingEmpty(): Boolean
