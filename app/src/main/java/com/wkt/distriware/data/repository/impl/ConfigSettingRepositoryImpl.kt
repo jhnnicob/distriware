6c04353732c6b28b93a72c5f37225011eaf2f601
Nico <molonjohnnico@gmail.com>
2023-07-05 22:07:25 +0800

    Change return to Flow

--- app/src/main/java/com/wkt/distriware/data/repository/impl/ConfigSettingRepositoryImpl.kt
+++ app/src/main/java/com/wkt/distriware/data/repository/impl/ConfigSettingRepositoryImpl.kt
@@ -3,29 +3,28 @@ package com.wkt.distriware.data.repository.impl
 import com.wkt.distriware.data.dataStore.ConfigSettingDataStore
 import com.wkt.distriware.data.repository.ConfigSettingRepository
 import com.wkt.distriware.domain.model.ConfigSetting
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 class ConfigSettingRepositoryImpl @Inject constructor(
     private val configSettingDataStore: ConfigSettingDataStore
 ): ConfigSettingRepository {
 
-    private val configSetting = ConfigSetting(
-        serverAddress = "192.168.1.12",
-        port = "8080"
-    )
+    override fun getConfigSetting(): Flow<ConfigSetting> {
+        return configSettingDataStore.getConfigSetting()
+    }
 
     override fun getConfigSetting(): ConfigSetting {
         return configSetting
     }
 
-    override fun isConfigSettingEmpty(): Boolean {
-        val serverAddress = configSetting.serverAddress ?: ""
-        val port = configSetting.port ?: ""
-        return serverAddress.isEmpty() && port.isEmpty()
-    }
-
-    override fun isConfigSettingValid(): Boolean {
-        val serverAddress = configSetting.serverAddress ?: ""
-        val port = configSetting.port ?: ""
-        return serverAddress == "192.168.17.12" && port == "8080"
+    override suspend fun isConfigSettingEmpty(): Flow<Boolean> {
+        val configSettingFlow = configSettingDataStore.getConfigSetting()
+        return configSettingFlow.map { configSetting ->
+            val serverAddress = configSetting.serverAddress
+            val port = configSetting.port
+            serverAddress?.isEmpty() ?: false && port?.isEmpty() ?: false
+        }
     }
 }
