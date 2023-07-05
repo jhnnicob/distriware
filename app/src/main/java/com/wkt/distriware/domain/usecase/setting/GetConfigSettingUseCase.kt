3bb7acc3d27cfd247973522a4b6fcccb033f7048
Nico <molonjohnnico@gmail.com>
2023-07-05 22:04:48 +0800

    Inject ConfigSettingRepository and change return to Flow of ConfigSetting

--- app/src/main/java/com/wkt/distriware/domain/usecase/setting/GetConfigSettingUseCase.kt
+++ app/src/main/java/com/wkt/distriware/domain/usecase/setting/GetConfigSettingUseCase.kt
@@ -1,12 +1,14 @@
 package com.wkt.distriware.domain.usecase.setting
 
 import com.wkt.distriware.data.repository.ConfigSettingRepository
-import com.wkt.distriware.data.repository.impl.ConfigSettingRepositoryImpl
 import com.wkt.distriware.domain.model.ConfigSetting
+import kotlinx.coroutines.flow.Flow
 
-class GetConfigSettingUseCase {
-    private val configSetting: ConfigSettingRepository = ConfigSettingRepositoryImpl()
-    operator fun invoke(): ConfigSetting {
+data class GetConfigSettingUseCase (
+    private val configSetting: ConfigSettingRepository
+) {
+
+    operator fun invoke(): Flow<ConfigSetting> {
         return configSetting.getConfigSetting()
     }
 }
