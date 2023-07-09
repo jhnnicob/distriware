2d04aeb64ac5cdefcf5778432a5e57e268783452
Nico <molonjohnnico@gmail.com>
2023-07-09 11:01:57 +0800

    Remove invoke function

--- app/src/main/java/com/wkt/distriware/domain/usecase/setting/ValidateConfigSettingEmptyUseCase.kt
+++ app/src/main/java/com/wkt/distriware/domain/usecase/setting/ValidateConfigSettingEmptyUseCase.kt
@@ -1,12 +1,9 @@
 package com.wkt.distriware.domain.usecase.setting
 
 import com.wkt.distriware.data.repository.ConfigSettingRepository
-import kotlinx.coroutines.flow.Flow
 
 class ValidateConfigSettingEmptyUseCase(
     private val configSettingRepository: ConfigSettingRepository
 ) {
-    suspend fun invoke(): Flow<Boolean> {
-        return configSettingRepository.isConfigSettingEmpty()
-    }
+
 }
