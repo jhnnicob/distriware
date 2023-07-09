11436a149de4d1cb7cd1aec35951095a297ae449
Nico <molonjohnnico@gmail.com>
2023-07-09 10:54:48 +0800

    Create generic datastore

--- app/src/main/java/com/wkt/distriware/data/dataStore/ConfigSettingDataStore.kt
+++ app/src/main/java/com/wkt/distriware/data/dataStore/ConfigSettingDataStore.kt
@@ -7,29 +7,29 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.wkt.distriware.constant.Constant
-import com.wkt.distriware.domain.model.ConfigSetting
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "config_settings")
 
+@Suppress("UNCHECKED_CAST")
 class ConfigSettingDataStore(private val context: Context) {
 
-    private val serverAddressKey = stringPreferencesKey(Constant.SERVER_ADDRESS_TEXT)
-    private val portNumberKey = stringPreferencesKey(Constant.PORT_NUMBER_TEXT)
-
-    suspend fun saveConfigSetting(key: String, value: String) {
+    suspend fun <T> saveDataStore(key: String, value: T) {
         val keyString = stringPreferencesKey(key)
+        val valueString = when (value) {
+            is List<*> -> (value as List<*>).joinToString(",")
+            else -> value.toString()
+        }
+
         context.dataStore.edit { preferences ->
-            preferences[keyString] = value
+            preferences[keyString] = valueString
         }
     }
 
-    fun getConfigSetting(): Flow<ConfigSetting> = context.dataStore.data
+    fun <T> getDataStore(key: String): Flow<T> = context.dataStore.data
         .map { preferences ->
-            ConfigSetting(
-                serverAddress = preferences[serverAddressKey],
-                port = preferences[portNumberKey],
-            )
+            val keyString = stringPreferencesKey(key)
+            preferences[keyString] as T
         }
 }
