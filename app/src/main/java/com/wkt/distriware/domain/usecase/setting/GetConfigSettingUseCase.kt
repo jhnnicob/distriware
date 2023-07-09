4e8e297c4cab3bb8c99bf8c485d0f09980f54495
Nico <molonjohnnico@gmail.com>
2023-07-09 10:56:42 +0800

    Change Flow type to String

--- app/src/main/java/com/wkt/distriware/domain/usecase/setting/GetConfigSettingUseCase.kt
+++ app/src/main/java/com/wkt/distriware/domain/usecase/setting/GetConfigSettingUseCase.kt
@@ -1,14 +1,13 @@
 package com.wkt.distriware.domain.usecase.setting
 
 import com.wkt.distriware.data.repository.ConfigSettingRepository
-import com.wkt.distriware.domain.model.ConfigSetting
 import kotlinx.coroutines.flow.Flow
 
 data class GetConfigSettingUseCase (
     private val configSetting: ConfigSettingRepository
 ) {
 
-    operator fun invoke(): Flow<ConfigSetting> {
-        return configSetting.getConfigSetting()
+    operator fun invoke(key: String): Flow<String> {
+        return configSetting.getDataStore(key)
     }
 }
